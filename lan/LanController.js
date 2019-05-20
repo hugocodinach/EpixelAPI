0d52a7570ad0e240cd60b15a0f278e8c3e5896dc
hugo codinach <hugo.codinach@epitech.eu>
2019-05-20 11:21:27 +0200

    [NEW] updateLan route

--- lan/LanController.js
+++ lan/LanController.js
@@ -1,9 +1,11 @@
 let express = require('express');
 let router = express.Router();
 let bodyParser = require('body-parser');
+let ObjectId = require('mongodb').ObjectID;
 
 router.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
@@ -28,6 +30,7 @@ router.post('/lan', function (req, res) {
         name: 'Lan',
         date: new Date(),
         games: [],
+        form: ''
     },
         function (err, lan) {
             if (err) return res.status(500).send({ msg: "There was a problem adding the information to the database." });
@@ -36,8 +39,20 @@ router.post('/lan', function (req, res) {
 });
 
 router.put('/lan', function (req, res) {
-    if (!req.body.name || !req.body.date)
-        return res.status(400).send({ msg: "Need a name and date." });
+    if (!req.body.name || !req.body.date || !req.body.games || !req.body.ObjectId)
+        return res.status(400).send({ msg: "Need a name, a date, games, and an ObjectId." });
+    Lan.update({ _id: ObjectId(req.body.ObjectId) }, {
+        name: req.body.name,
+        date: req.body.date,
+        games: req.body.games
+    }, function (err, lan) {
+        if (err) return res.status(500).send({ msg: "There was a problem adding the information to the database." });
+        Lan.find({}, function (err, lans) {
+            if (err) return res.status(500).send("There was a problem finding games.");
+            if (!lans[0]) return res.status(200).send({});
+            res.status(200).send(lans[0]);
+        });
+    });
 });
 
 module.exports = router;
